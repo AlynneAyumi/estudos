d95c27f091fdd692f804dbaa85e16c08a9fcf9e4
AlynneAyumi <alynneak@outlook.com>
2024-09-25 20:38:48 -0300

    CommitFinalizado

--- ExemploDOM/script.js
+++ ExemploDOM/script.js
@@ -1,3 +1,42 @@
+function validarCPF(cpf) {
+    // Remove caracteres não numéricos
+    cpf = cpf.replace(/[^\d]+/g, '');
+  
+    // Verifica se tem 11 dígitos
+    if (cpf.length !== 11) {
+      return false;
+    }
+  
+    // Valida os dígitos verificadores
+    let soma = 0;
+    let resto;
+    for (let i = 1; i <= 9; i++) {
+      soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+      resto = 0;
+    }
+    if (resto !== parseInt(cpf.substring(9, 10))) {
+      return false;
+    }
+  
+    soma = 0;
+    for (let i = 1; i <= 10; i++) {
+      soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+      resto = 0;
+    }
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+      return false;
+    }
+  
+    return true;
+  }
+
+
 // Exibir os dados do formulário na tela
 function exibirDados(person) {
     
@@ -12,8 +51,8 @@ function exibirDados(person) {
 };
 
 // Envio do formulário
-document.getElementById('formulario').addEventListener('submit', function() {
-
+document.getElementById('formulario').addEventListener('submit', function(e) {
+    e.preventDefault();
     let nome = document.getElementById('nome').value;
     let sobrenome = document.getElementById('sobrenome').value;
     let CPF = document.getElementById('CPF').value;
@@ -22,7 +61,12 @@ document.getElementById('formulario').addEventListener('submit', function() {
     // Objeto
     const pessoa = {nome, sobrenome, CPF, senha};
 
-    exibirDados(pessoa);
+    if (validarCPF(pessoa.CPF)) {
+        exibirDados(pessoa);
+    } else {
+        alert(`CPF inválido!`);
+    }
 });
 
+exibirDados();
 
